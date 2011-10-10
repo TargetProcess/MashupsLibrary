09977b311df23a905e048d79cda9940f544b2a35
Alex Fomin <Alex.Fomin@targetprocess.com>
2011-10-10 17:47:43 +0300

    Update NumberOfAssignments to be shown on each kanban area correctly

--- NumberOfAssignments/NumberOfAssignments.js
+++ NumberOfAssignments/NumberOfAssignments.js
@@ -9,10 +9,10 @@ tau.mashups
 					var assignment = resp.Items[x].Assignments[y];
 					var id = assignment.GeneralUser.Id;
 					if (map[id]) {
-						map[id]++;
+						map[id].count++;
 					}
 					else {
-						map[id]=1;
+						map[id]={name:assignment.GeneralUser.FirstName+' '+assignment.GeneralUser.LastName, count:1};
 					}
 				}
 			}
@@ -22,7 +22,7 @@ tau.mashups
 		var getAssignments = function(projectName, entityName, callback){
 			$.ajax({
                 type: 'GET',
-                url: appHostAndPath+'/api/v1/'+entityName+'?where=(EntityState.IsFinal eq "false") and (EntityState.IsInitial eq "false") and (EntityState.Name ne "Planned") and (EntityState.Name ne "Merged") and (Project.Name eq "'+projectName+'")&include=[Assignments[GeneralUser[id]]]&take=1000&format=json',
+                url: appHostAndPath+'/api/v1/'+entityName+'?where=(EntityState.IsFinal eq "false") and (EntityState.IsInitial eq "false") and (EntityState.Name ne "Planned") and (EntityState.Name ne "Merged") and (Project.Name eq "'+escape(projectName)+'")&include=[Assignments[GeneralUser[id,FirstName,LastName]]]&take=1000&format=json',
                 contentType: 'application/json',
                 dataType: 'json',
                 success: callback
@@ -35,39 +35,44 @@ tau.mashups
 					if (join.hasOwnProperty(key))
 					{
 						joined[key]= joined[key] || {};
-						joined[key][propertyName] = join[key];
+//name, count
+	
+						joined[key].name = join[key].name;
+						joined[key][propertyName] = join[key].count;
 					}
 				}
 		};
 		
-		var projectName = jQuery.trim($('.x-panel-kanban-header-link')[0].text);
+
+		var createAssignmentsList= function(index, element){
+		var projectName = jQuery.trim($(element).find('.x-panel-kanban-header-link').text());
 		var userStories = {};
 		var bugs = {};
-
 		getAssignments(projectName, 'UserStories', function(resp){
-			userStories = processResponse(resp);
-			getAssignments(projectName, 'Bugs', function(resp){
-				bugs = processResponse(resp);
-
-				var joined = {};
-				merge(joined,userStories,'usCount');
-				merge(joined,bugs,'bugsCount');
-				
-				debugger;
-				var test=['<div><div style="text-align: center;">'];
-				for (var key in joined)
-				{
-					if (joined.hasOwnProperty(key)){
-						test.push('<span style="display: inline-block; position: relative; width: 80px;">');
-						test.push('<img src="../../../avatar.ashx?size=30&UserId='+key+'"/>')
-						test.push('<span style="position: absolute; top: 0pt; right: 0pt;"><img src="../../../img/BugS.gif"/>'+(joined[key].bugsCount || 0)+'</span>');
-						test.push('<span style="position: absolute; bottom: 0pt; right: 0pt;"><img src="../../../img/UserStoryS.gif"/>'+(joined[key].usCount || 0)+'</span>');
-						test.push('</span>');
+				userStories = processResponse(resp);
+				getAssignments(projectName, 'Bugs', function(resp){
+					bugs = processResponse(resp);
+	
+					var joined = {};
+					merge(joined,userStories,'usCount');
+					merge(joined,bugs,'bugsCount');
+					
+					var test=['<div><div style="text-align: center;">'];
+					for (var key in joined)
+					{
+						if (joined.hasOwnProperty(key)){
+							test.push('<span style="display: inline-block; position: relative; width: 80px;">');
+							test.push('<img src="../../../avatar.ashx?size=30&UserId='+key+'" title="'+joined[key].name+'"></img>')
+							test.push('<span style="position: absolute; top: 0pt; right: 0pt;"><img src="../../../img/BugS.gif"/>'+(joined[key].bugsCount || 0)+'</span>');
+							test.push('<span style="position: absolute; bottom: 0pt; right: 0pt;"><img src="../../../img/UserStoryS.gif"/>'+(joined[key].usCount || 0)+'</span>');
+							test.push('</span>');
+						}
 					}
-				}
-				test.push('</div></div>');
-				var output=test.join('');
-				$('.x-panel-kanban-header').append(output);
-			});
+					test.push('</div></div>');
+					var output=test.join('');
+					$(element).append(output);
+				});
 		});
+		};
+		$('.x-panel-kanban-header').each(createAssignmentsList);
 	});
