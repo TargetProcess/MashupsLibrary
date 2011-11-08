cdb99d57a0533f0c4a427bda2c367842f8df51b4
Jonathan Enzinna <jonnyfunfun@gmail.com>
2011-11-08 09:11:16 -0500

    Added comments to DetailsHider JS, updated README

--- DetailsHider/DetailsHider.js
+++ DetailsHider/DetailsHider.js
@@ -4,8 +4,12 @@ tau.mashups
 
 		function DetailsHider() {}
         $(document).ready(function() { 
+            /* Hide the page generation time */
             $('#copy').find('div').first().hide();
-            $('#copy').parent().css('font-size','0'); /* such a terrible hack */
+            /* Hide the "You're using the free version" text
+             * (this is such a terrible hack, but the text isn't wrapped in its own <div> or anything */
+            $('#copy').parent().css('font-size','0');
+            /* And hide the "what's new" and "check for updates" line */
             $('#ctl00_footer_notLoginPage').hide()
         });
     }
