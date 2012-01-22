495f5e69fb906aff69d528674fa93a3505a3495c
Michael Dubakov <mdubakov@gmail.com>
2012-01-23 01:09:28 +0200

    UI improved: Add button, success message

--- QuickAdd/QuickAdd.js
+++ QuickAdd/QuickAdd.js
@@ -4,7 +4,7 @@
 tau.mashups
 .addDependency('libs/jquery/jquery')
 .addMashup(function ($, config) {
-	$('#main').prepend("<div id='m_quickAddBlock' style='margin-left:10px'><a href='#' id='m_quickAddLink'>Quick Add</a></div>");
+	$('#main').prepend("<div id='m_quickAddBlock' style='width: 400px;'><a href='#' id='m_quickAddLink'>Quick Add</a></div><br style='clear:both'>");
 
 	function getQueryParams(qs) {
 		qs = qs.split("+").join(" ");
@@ -44,8 +44,8 @@ tau.mashups
 	// quick add
 	$("#m_quickAddLink").live("click", function(){
 		
-		var addAction = $("<a id='m_addEntityAction' href='#' class='btn small primary' style='margin: 0 10px; float: left'>Add</a> <a href='#' class='btn small' id='cancelAdd'>Cancel</a>");
-		var addText = $('<textarea id="addText" rows="3" cols="60" style="float: left; margin-bottom: 10px"></textarea><br style="clear:both">');
+		var addAction = $("<div class='main-controls left'><button type='button' class='button' id='m_addEntityAction' style='margin-right: 10px'>Add</button> <a href='#' style='margin-top: 5px' id='cancelAdd'>Cancel</a></div>");
+		var addText = $('<span class="secondaryInfo">You can add several Stories, Features and Bugs at once. For example:<br>Feature: super search<br>Bug: something wrong<br>Story: As a bee I want to fly</span><br><textarea id="addText" rows="4" cols="120" style="margin-bottom: 10px"></textarea>');
 		addText.val("Story: ");
 		
 		
@@ -77,8 +77,8 @@ tau.mashups
 							contentType: 'application/json',
 							data: JSON.stringify({Name:value.Name, Project:{Id:projectId}})
 						}).done(function( msg ) {
-							$("#m_quickAddBlock").prepend("<div id='savedOK' style='background: yellow'>Entity saved: "+msg.Name+"</div>");
-							$("#savedOK").fadeOut(2000)
+							$("#m_quickAddBlock").prepend("<div id='savedOK' style='color: #000; text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5); padding: 3px; background: #A5C956; border: 1px solid #A5C956;'>Entity saved: "+msg.Name+"</div>");
+							$("#savedOK").fadeOut(3000)
 						});
 					});
 					                            
