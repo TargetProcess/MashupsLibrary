c74a5dcb43225ba014e144f1f1c3a2d828300f20
Jonathan Enzinna <jonnyfunfun@gmail.com>
2012-04-04 10:57:19 -0400

    Adding Assigned Effort Report

---
+++ Assigned Effort Report/Assigned Effort Report.js
@@ -0,0 +1,114 @@
+tau.mashups
+    .addDependency('libs/jquery/jquery').addMashup(function() {
+
+        function showReport() {
+            $('head').append('<style type="text/css"> \
+                table.board-efforts tr.hoverHi:hover {background: #E3F5D7 !important} \
+                table.board-efforts tr {border-bottom: 1px dotted #eee !important} \
+                table.board-efforts td {height: 20px; } \
+                table.board-efforts td em {font-size: smaller;} \
+                table.board-efforts {border-collapse: collapse} \
+                table.board-efforts td.more {background: url("'+appHostAndPath+'/img/icons/plus-small.png") center center no-repeat; width: 10px; } \
+                table.board-efforts td.more.less {background: url("'+appHostAndPath+'/img/icons/minus-small.png") center center no-repeat !important;} \
+                table.board-efforts td.bar {background: #ACD473; margin-top: 2px; border: 1px solid #8ACB29; color: white; overflow: hidden; position: absolute; font: 11px Arial; height: 12px; border-radius: 3px; padding: 2px; color: white; font-weight: bold; font-size: 11px;} \
+                table.board-efforts tr.innerData {height: 0px; overflow: hidden; display: none;} \
+                table.board-efforts-inner {float: right; width: 90%;} \
+                </style> \
+            ');
+            $('td.col-two > div:first').fadeOut('slow', 
+                function() { 
+                    $(this).html('').append(
+                        $('<span class="tableTitle">Total effort assigned to users</span><br/><br/><div id="assigned-effort-report"><div class="ui-wait-icon"></div></div>')
+                    ).fadeIn('slow', loadData);
+                }
+            );
+        }
+
+        var api_base = appHostAndPath+"/api/v1/";
+
+        function loadData() {
+            $.getJSON(api_base+"Assignables?include=[Assignments[GeneralUser[FirstName,LastName,Role],Role],RoleEfforts[Effort,Role],EntityType,EntityState,Name]&where=(EntityState.IsFinal%20eq%20'false')%20and%20(Effort%20gt%200)&format=json&take=1000", formatResult);
+        }
+
+        function formatResult(data) {
+            console.log(data);
+            var formattedData = {'users': {}, 'overallEffort': 0};
+            $.each(data.Items, function(k,item) {
+                $.each(item.Assignments.Items, function(l,assignment) {
+                    /* check to see if this user is in our formattedData object */
+                    if (formattedData.users[assignment.GeneralUser.Id] == null) {
+                        /* create the first record */
+                        formattedData.users[assignment.GeneralUser.Id] = {
+                            'FirstName'     : assignment.GeneralUser.FirstName,
+                            'LastName'      : assignment.GeneralUser.LastName,
+                            'DefaultRole'   : assignment.GeneralUser.Role.Name,
+                            'TotalEffort'   : 0,
+                            'Items'         : []
+                        }
+                    }
+                    var roleEffort = getRoleEffort(item, assignment.Role.Id);
+                    formattedData.users[assignment.GeneralUser.Id].TotalEffort += roleEffort;
+                    formattedData.overallEffort += roleEffort;
+                    formattedData.users[assignment.GeneralUser.Id].Items.push({
+                        'EntityId'      : item.Id,
+                        'Name'          : item.Name,
+                        'EntityType'    : item.EntityType.Name,
+                        'EntityState'   : item.EntityState.Name,
+                        'Role'          : assignment.Role.Name,
+                        'Effort'        : roleEffort
+                    });
+                });
+            });
+            drawChart(formattedData);
+        }
+
+        function getRoleEffort(item, roleId) {
+            for (var i = 0; i < item.RoleEfforts.Items.length; ++i) {
+                if (item.RoleEfforts.Items[i].Role.Id == roleId)
+                    return item.RoleEfforts.Items[i].Effort;
+            }
+        }
+
+        function drawChart(data) {
+            var table = $('<table class="board-efforts" style="width: 100%;"></table>');
+            table.append($('<tr><th colspan="2" style="width: 25%;">User</th><th style="width: 75%;">Total Effort</th></tr>'));
+            $.each(data.users, function(k,user) {
+                var tr = $('<tr class="hoverHi"></tr>');
+                tr.append($('<td class="more"></td>').click(function() {
+                    /* this should open the "more information thing */
+                    $(this).toggleClass('less');
+                    $(this).parent().next().slideToggle('slow');
+                }));
+                tr.append("<td>{0}, {1} <em>({2})</em></td>".f(user.FirstName, user.LastName, user.DefaultRole));
+                var width = (user.TotalEffort / data.overallEffort) * 100;
+                tr.append($('<td></td>').html(user.TotalEffort).addClass('bar').css('width', width+"%"));
+                table.append(tr);
+                /* build the expanded data row */
+                tr = $('<tr class="innerData"></tr>');
+                var inner = $('<td></td>').attr('colspan','3');
+                var innerTable = $('<table class="board-efforts-inner"></table>');
+                innerTable.append($('<tr><th style="width: 33%;" colspan="2">Assignable</th><th style="width: 10%">State</th><th style="width: 10%">Role</th><th>Assigned Effort</th></tr>'));
+                $.each(user.Items, function(k,item) {
+                    innerTable.append('<tr><td><img src="{0}/img/{1}.gif"></td><td>{2}</td><td>{3}</td><td>{4}</td><td>{5}</td>'.f(appHostAndPath, item.EntityType, item.Name, item.EntityState, item.Role, item.Effort));
+                });
+                inner.append(innerTable);
+                tr.append(inner);
+                table.append(tr);
+            });
+            $('div#assigned-effort-report').html('').append(table);
+            console.log(data);
+        }
+
+        /* add the link */
+        $(document).ready(function() {
+            $('a:contains("Time By Person")').after($('<a id="allocation-link" href="#">Assigned Effort</a>').click(showReport));
+        });
+    }
+);
+
+String.prototype.f = function() {
+    var s = this, i = arguments.length;
+    while (i--)
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    return s;
+};
