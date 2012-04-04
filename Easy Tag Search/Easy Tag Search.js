200f9e96766428c68a5dd70cdf6f2925d10d28dd
Jonathan Enzinna <jonnyfunfun@gmail.com>
2012-04-04 11:38:19 -0400

    Adding Easy Tag Search mashup

---
+++ Easy Tag Search/Easy Tag Search.js
@@ -0,0 +1,24 @@
+tau.mashups
+	.addDependency("libs/jquery/jquery")
+	.addMashup(function(config) {
+
+        function searchifyTags() {
+            $('li[rel="tag"]:not(".search-linked")').each(function() {
+                $(this).addClass('search-linked').find('span:eq(0)').html('<a href="{0}/Search/Search.aspx?SearchString=tag%253a{1}">{1}</a>'.f(
+                    appHostAndPath, $(this).find('span:eq(0)').html()));
+            });
+        }
+
+        $('#main').bind('DOMSubtreeModified', searchifyTags);
+        $(document).ready(searchifyTags);
+    }
+);
+
+/* my new favorite proto function */
+String.prototype.f = function() {
+    var s = this, i = arguments.length;
+    while (i--)
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    return s;
+};
+
