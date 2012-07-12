f03a1a0176ccb695f78a9d56454cd23e0ad692ef
Oleg <oleg.seriaga@gmail.com>
2012-07-12 17:02:30 +0300

    remove subscription on DOM modifications since it slows down performance

--- Auto Reply Customer Requests/Auto Reply Customer Requests.js
+++ Auto Reply Customer Requests/Auto Reply Customer Requests.js
@@ -4,15 +4,11 @@ tau.mashups
         function checkRequestersCheckBoxes() {
             /* old views */
             $('div.notify').find('input.requesters').prop('checked', true);
-            /* new views */
-            $('div.tau-notification-practices-settings').find('input#requestor').prop('checked', true);
         };
 
         $(document).ready(function() {
             /* firing on ready works for old views */
             checkRequestersCheckBoxes();
-            /* and we need to do this for new views */ 
-            $('#main').bind('DOMSubtreeModified', function() { checkRequestersCheckBoxes() }); // TODO: this is super ugly and should probably change
         });
     }
 );
