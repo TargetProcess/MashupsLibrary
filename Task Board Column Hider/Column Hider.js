ac2eaf53b1abfb252fd63adba22b9eb1bae1e8b1
Jonathan Enzinna <jonnyfunfun@gmail.com>
2012-08-08 08:46:03 -0400

    Adding Task Board Column Hider

---
+++ Task Board Column Hider/Column Hider.js
@@ -0,0 +1,21 @@
+tau.mashups
+ .addDependency('libs/jquery/jquery')
+ .addMashup(function ($, config) {
+   
+   // Put states' names that you want to hide here
+   var statesToHide = ["Testing"];
+   
+   for (var i = 0; i < statesToHide.length; i++) {
+    var header = $(".kanban-swimlane-header-wrap span:contains('"+statesToHide[i]+"')").filter(function() {
+        return $(this).text().match("^"+statesToHide[i]) != null;
+    }).parent();
+
+    //use header Id to construct column Id
+    var col = $("#" + header.id().replace("header-", ""));
+
+    //hide everything
+    header.hide();
+    col.hide();
+   }
+   
+});
