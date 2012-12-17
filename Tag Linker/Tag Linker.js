ed251bb1c81f42cef412d70fa47a0ea660e916e7
Jonathan Enzinna <jonnyfunfun@gmail.com>
2012-12-17 09:04:58 -0500

    Fixed tag linker render issues, related to #15

--- Tag Linker/Tag Linker.js
+++ Tag Linker/Tag Linker.js
@@ -6,37 +6,38 @@ tau.mashups
 	.addDependency("jQuery")
 	.addMashup(function(storyView, taskView, requestView, bugView, $, config) {
 
-        
        function renderTagLinks() {
-            var API_BASE = appHostAndPath + '/api/v1/Generals/{0}?include=[EntityType]&format=json';
-            $('li[rel="tag"]:not(".linkz0red")').each(function() {
-                $(this).addClass('linkz0red');
-                var capture = /#(\d+)/.exec($(this).find('span:eq(0)').html());
-                if (capture != null) {
-                    var id = capture[1];
-                    $.ajax({
-                        type: 'GET',
-                        url: API_BASE.f(id),
-                        context: $(this)[0],
-                        contentType: 'application/json',
-                        dataType: 'json',
-                        success: function(resp) {
-                            $(this).find('span:eq(0)').html("<a href='#{0}/{1}'>{2}</a>".f(
-                                    resp.EntityType.Name.toLowerCase(), resp.Id,  
-                                    $(this).find('span:eq(0)').html())
-                                );
-                        }
-                    });
-                };
-            });
+			window.setTimeout(function() {
+	            $('li[rel="tag"]:not(".linkz0red")').each(function() {
+					console.log($(this));
+	                $(this).addClass('linkz0red');
+	                var capture = /#(\d+)/.exec($(this).find('span:eq(0)').html());
+	                if (capture != null) {
+	                    var id = capture[1];
+	                    $.ajax({
+	                        type: 'GET',
+	                        url: (appHostAndPath + '/api/v1/Generals/{0}?include=[EntityType]&format=json').f(id),
+	                        context: $(this)[0],
+	                        contentType: 'application/json',
+	                        dataType: 'json',
+	                        success: function(resp) {
+	                            $(this).find('span:eq(0)').html("<a href='#{0}/{1}'>{2}</a>".f(
+	                                    resp.EntityType.Name.toLowerCase(), resp.Id,  
+	                                    $(this).find('span:eq(0)').html())
+	                                );
+	                        }
+	                    });
+	                };
+	            });
+			},1000);
         }
+
+        $(document).ready(renderTagLinks);
 		
 		storyView.onRender(renderTagLinks);
 		taskView.onRender(renderTagLinks);
 		requestView.onRender(renderTagLinks);
 		bugView.onRender(renderTagLinks);
-
-        $(document).ready(renderTagLinks);
     }
 );
 
