779e5b0e66f654c6158419c830be92fb1bfd4f2f
AndreyVaskovskiy <andrey.vaskovskiy@targetprocess.com>
2012-12-24 16:05:37 +0300

    #Added skipped Request types array to skip showing prompt pop-up for some types of requests (like not to show for 'Idea' request type)

--- Leave Request In Queue/Leave Request In Queue.js
+++ Leave Request In Queue/Leave Request In Queue.js
@@ -1,44 +1,31 @@
 tau.mashups
 	.addDependency('app.path')
-	.addDependency('Underscore')
+	.addDependency('tau/core/global.bus')
+	.addDependency('tau/configurator')
 	.addMashup(
-		function (appPath, _, config) {
+		function (appPath, gb, c, config) {
+			var store;
 			var typeById = {};
-			var nativeRequire = require;
-			var waitForBus = function () {
-				require(['tau/core/global.bus'], function (gb) {
-					gb.get().on('beforeInit', function (e) {
-						if (e.data.config && e.data.config.entity && !typeById[e.data.config.entity.id]) {
-							for (var prop in typeById) {
-								if (typeById.hasOwnProperty(prop))
-									delete typeById[prop];
-							}
-							typeById[e.data.config.entity.id] = e.data.config.entity.type;
-						}
-					});
-				});
+			if (!store) {
+				store = c.getStore();
 			}
-			window.require = function () {
-				if (require.defined('all.components')) {
-					window.require = nativeRequire;
-					waitForBus();
+			gb.get().on('beforeInit', function (e) {
+				if (e.data.config && e.data.config.entity && !typeById[e.data.config.entity.id]) {
+					for (var prop in typeById) {
+						if (typeById.hasOwnProperty(prop))
+							delete typeById[prop];
+					}
+					typeById[e.data.config.entity.id] = e.data.config.entity.type;
 				}
-				nativeRequire.apply(this, arguments);
-				if (arguments[0][0] == 'all.components') {
-					window.require = nativeRequire;
-					require(['all.components'], function () {
-						waitForBus();
-					});
-				}
-			};
-			_.extend(window.require, nativeRequire);
+			});
 
 			var requestAddCommentHook = function (placeholder) {
 				this.theOptions = {
-					question: "Leave Request in Queue?",
-					yesAnswer: " Leave ",
-					cancelAnswer: "Remove, as usual"
+					question: 'Leave Request in Queue?',
+					yesAnswer: ' Leave ',
+					cancelAnswer: 'Remove, as usual'
 				};
+				this.skipedTypes = ['Idea'];
 				this.requestViewUrl = appPath.get() + '/Project/HelpDesk/Request/View.aspx';
 				this.soapCommentUrl = (appPath.get() + '/Services/CommentsControl.asmx/Create').replace(location.protocol + '//' + location.host, '');
 				this.restCommentUrl = appPath.get() + '/api/v1/comments.asmx/?';
@@ -78,66 +65,91 @@ tau.mashups
 						questionHolder = d.comment.ParentID ? $('button:contains("Reply").ui-add-comment') : $('button:contains("Comment").ui-add-comment');
 					}
 					if (questionHolder && questionHolder.length === 1) {
-						var q = $(questionHolder[0]), successCallback, errorCallback;
-						if (q.next('.tau-bubble').length <= 0) {
-							var opt = this.theOptions;
-							q.after('<div class="tau-bubble" style="z-index: 1; display: block;">' +
-								'<div class="tau-bubble__inner" style="margin: 0 0 -1px !important;"><div style="padding: 10px;"><p style="margin:0px !important">' + opt.question +
-								'</p><div class="button-group"><button class="button primary" type="button">' + opt.yesAnswer +
-								'</button><button class="button" type="button">' + opt.cancelAnswer + '</button></div></div></div>' +
-								'<div class="tau-bubble__arrow" data-orientation="bottom" style="display: block;"></div></div>');
-							var b = q.next('.tau-bubble');
-							var placeholder = $(this.placeholder);
-							var f = $.proxy(function (e, xhr, settings) {
-								if (settings.isRequestNotReplied != null && settings.isRequestNotReplied === true) {
-									placeholder.off('ajaxSuccess', f);
-									var generalId, d = JSON.parse(settings.data);
-									if (this._isRestCommentCreatedForRequest(settings, d)) {
-										generalId = d.general.id;
-									} else if (this._isSoapCommentCreatedForRequest(settings, d)) {
-										generalId = d.comment.GeneralID;
+						var successCallback, errorCallback, showQuestion = $.proxy(function (q) {
+							if (q.next('.tau-bubble').length <= 0) {
+								var opt = this.theOptions;
+								q.after('<div class="tau-bubble" style="z-index: 1; display: block;">' +
+									'<div class="tau-bubble__inner" style="margin: -2px 1px -2px 1px !important;"><div style="padding: 10px;"><p style="margin:0px !important">' + opt.question +
+									'</p><div class="button-group"><button class="button primary" type="button">' + opt.yesAnswer +
+									'</button><button class="button" type="button">' + opt.cancelAnswer + '</button></div></div></div>' +
+									'<div class="tau-bubble__arrow" data-orientation="bottom" style="display: block;"></div></div>');
+								var b = q.next('.tau-bubble');
+								var placeholder = $(this.placeholder);
+								var f = $.proxy(function (e, xhr, settings) {
+									if (settings.isRequestNotReplied != null && settings.isRequestNotReplied === true) {
+										placeholder.off('ajaxSuccess', f);
+										var generalId, d = JSON.parse(settings.data);
+										if (this._isRestCommentCreatedForRequest(settings, d)) {
+											generalId = d.general.id;
+										} else if (this._isSoapCommentCreatedForRequest(settings, d)) {
+											generalId = d.comment.GeneralID;
+										}
+										$.ajax({
+											url: appPath.get() + '/api/v1/Requests/' + generalId,
+											data: JSON.stringify({ IsReplied: false }),
+											headers: { 'Content-Type': 'application/json' },
+											success: function () { },
+											error: function () { },
+											dataType: 'json',
+											type: 'POST'
+										});
 									}
-									$.ajax({
-										url: appPath.get() + '/api/v1/Requests/' + generalId,
-										data: JSON.stringify({ IsReplied: false }),
-										headers: { 'Content-Type': 'application/json' },
-										success: function () { },
-										error: function () { },
-										dataType: 'json',
-										type: 'POST'
-									});
 								}, this);
-							}
-							if (isRestComment) {
-								var c = $('div.ui-all-comments');
-								if (c.children().length === 1) {
-									var h = c.height();
-									c.height(b.height() + h);
-									questionHolder.css('top', h + 10);
+								if (isRestComment) {
+									var c = $('div.ui-all-comments');
+									if (c.children().length === 1) {
+										var h = c.height();
+										c.height(b.height() + h);
+										questionHolder.css('top', h + 10);
+									}
 								}
+								var p = q.position();
+								b.css('top', (p.top - b.height() - ((isRestComment && d.parentId) ? $('div.ui-richeditor').height() : 0)) + 'px').css('left', p.left + 'px');
+								b.find('button').bind('click', $.proxy(function (e) {
+									options.postponed = false;
+									options.isRequestNotReplied = $(e.target).is('.primary');
+									if (options.isRequestNotReplied === true) {
+										placeholder.ajaxSuccess(f);
+									}
+									$.ajax(options).success(successCallback).error(errorCallback);
+									this.fadeOut(300, function () {
+										$(this).remove();
+									})
+								}, b));
+								q.next('.tau-bubble').css('visibility', 'visible').animate({ opacity: 1 }, 300);
 							}
-							var p = q.position();
-							b.css('top', (p.top - b.height() - ((isRestComment && d.parentId) ? $('div.ui-richeditor').height() : 0)) + 'px').css('left', p.left + 'px');
-							b.find('button').bind('click', $.proxy(function (e) {
-								options.postponed = false;
-								options.isRequestNotReplied = $(e.target).is('.primary');
-								if (options.isRequestNotReplied === true) {
-									placeholder.ajaxSuccess(f);
+						}, this);
+						if (isRestComment) {
+							store.evictProperties(d.general.id, 'request', ['requestType']);
+							store.get('request', { id: d.general.id, fields: [{ 'requestType': ['id', 'name'] }] }, {
+								success: function (res) {
+									if ($.inArray(res.data.requestType.name, r.skipedTypes) === -1) {
+										showQuestion($(questionHolder[0]));
+									}
+									else {
+										options.postponed = false;
+										$.ajax(options).success(successCallback).error(errorCallback);
+									}
 								}
-								$.ajax(options).success(successCallback).error(errorCallback);
-								this.fadeOut(300, function () {
-									$(this).remove();
-								})
-							}, b));
+							}).done();
+						} else if (isSoapComment) {
+							showQuestion($(questionHolder[0]));
 						}
-						q.next('.tau-bubble').css('visibility', 'visible').animate({ opacity: 1 }, 300);
 						jqXHR.success = function (callback) {
-							successCallback = callback;
-							return this;
+							if (options.postponed) {
+								successCallback = callback;
+								return this;
+							} else {
+								$.ajax(options).success(callback);
+							}
 						};
 						jqXHR.error = function (callback) {
-							errorCallback = callback;
-							return this;
+							if (options.postponed) {
+								errorCallback = callback;
+								return this;
+							} else {
+								$.ajax(options).error(callback);
+							}
 						};
 						return {
 							send: function () { },
