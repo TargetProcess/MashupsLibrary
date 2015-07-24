a61311923a913f124e8c43bcd6d3bacd10c140ef
Julia Khmylova <julia.khmylova@targetprocess.com>
2015-07-24 20:56:35 +0300

    Add all available options

--- Auto Reply Customer Requests for TP3/Auto Reply Customer Requests.js
+++ Auto Reply Customer Requests for TP3/Auto Reply Customer Requests.js
@@ -5,6 +5,9 @@ tau.mashups
            bus.on('$editorHolder.ready', function (evt, $element) {
                setTimeout(function () {
                    $element.find('input[value=Requesters]').prop('checked', true);
+                  //$element.find('input[value=Owner]').prop('checked', true);
+                  //$element.find('input[value=Assigned]').prop('checked', true);
+                  //$element.find('input[value=Team]').prop('checked', true);
                }, 0);
            })
        })
