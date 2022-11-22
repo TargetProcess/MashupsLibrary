73c98943f457f55ea752065f26759f86dfeb2f83
Natalia Dulko <ms.duna@gmail.com>
2022-11-22 10:28:48 +0100

    Add files via upload
    Moving obsolete mashups from TP3MashupLibrary

--- Entity Templates/EntityTemplates.js
+++ Entity Templates/EntityTemplates.js
@@ -1,50 +1,97 @@
-tau.mashups.addDependency('libs/jquery/jquery').addMashup(
-function (config) {
-    function entityTemplates() {}
-        entityTemplates.prototype = {
-            render: function () {
-                /* find what page we're on and proceed accordingly */
-                switch (tau.mashups.currentPlaceholder) {
-                    case 'Project_Planning_UserStory_Edit':
-                        this.getTemplateAndPlace('UserStory','ctl00_mainArea_usEdit_UsEdit_DescriptionTextBox_ckeditor');
-                        break;
-                    case 'Project_Planning_Feature_Edit':
-                        this.getTemplateAndPlace('Feature','ctl00_mainArea_featureDetails_FeatureDetails_DescriptionTextBox_ckeditor');
-                        break;
-                    case 'Project_Planning_Task_Edit':
-                        this.getTemplateAndPlace('Task','ctl00_mainArea_taskDetails_taskDetails_DescriptionTextBox_ckeditor');
-                        break;
-                    case 'Project_HelpDesk_Request_Edit':
-                        this.getTemplateAndPlace('Request','ctl00_mainArea_RequestDetails_RequestDetails_DescriptionTextBox_ckeditor');
-                        break;
-                    case 'Project_QA_Bug_Edit':
-                        this.getTemplateAndPlace('Bug','ctl00_mainArea_bugDetails_BugDetails_DescriptionTextBox_ckeditor');
-                        break;
-                    case 'Project_QA_TestCase_Edit':
-                        this.getTemplateAndPlace('TestCaseSteps','ctl00_mainArea_be_TestCaseDetails_Steps_ckeditor');
-                        this.getTemplateAndPlace('TestCaseSuccess','ctl00_mainArea_be_TestCaseDetails_Success_ckeditor');
-                        break;
+tau.mashups
+    .addDependency('Underscore')
+    .addDependency('tau/configurator')
+    .addMashup(function(_, configurator) {
+
+        'use strict';
+
+        var templates = {
+            'UserStory': 'As a <i>type of user</i>, I want <i>some goal</i> so that <i>some reason</i>',
+            'Feature': '<table border="1"><caption>Features and Benefits matrix</caption>' +
+                '<thead><tr><th scope="col">Features</th><th scope="col">Benefits</th></tr></thead>' +
+                '<tbody><tr>' +
+                '<td><em>A short phrase, giving a name and some implied context to the feature</em></td>' +
+                '<td><em>A short description which describes the benefit to the user and the business. There may be multiple benefits per feature which are highlighted here</em></td>' +
+                '</tr>' +
+                '<tr><td><em>...</em></td><td><em>...</em></td></tr>' +
+                '<tr><td>...</td><td>...</td></tr>' +
+                '</tbody></table>',
+            'Task': null,
+            'Request': null,
+            'Bug': '', //put an HTML-formatted template into quotes
+            'TestCase': ''
+        };
+
+        var reg = configurator.getBusRegistry();
+
+        function addBusListeners(busName, events) {
+            var scope = {};
+
+            reg.on('create', function(e, data) {
+                var bus = data.bus;
+                if (bus.name === busName) {
+                    _.each(events, function(listener, eventName) {
+                        bus.once(eventName, listener, scope);
+                    });
+                }
+            });
+
+            reg.on('destroy', function(e, data) {
+                var bus = data.bus;
+                if (bus.name === busName) {
+                    bus.removeAllListeners(scope);
+                }
+            });
+        }
+
+        function findTemplate(entityTypeName) {
+            return _.find(templates, function(v, k) {
+                return k.toLowerCase() === entityTypeName;
+            });
+        }
+
+        function getTemplate(context) {
+            var entityTypeName = context.entity.entityType.name.toLowerCase();
+            var term = _.find(context.getTerms(), function(v) {
+                return (v.wordKey || v.name).toLowerCase().replace(' ', '') === entityTypeName;
+            });
+            var template = term ? findTemplate(term.value.toLowerCase()) : null;
+            return template || findTemplate(entityTypeName);
+        }
+
+        addBusListeners('description', {
+            'afterRender': function(e, renderData) {
+                var value = renderData.data.value;
+                if (value) {
+                    return;
+                }
+
+                var template = getTemplate(renderData.view.config.context);
+                if (!template) {
+                    return;
+                }
+
+                var $description = renderData.element.find('.ui-description__inner');
+                if ($description.length) {
+                    $description.attr('data-placeholder', '');
+                    $description.append('<div>' + template + '</div>');
                 }
             },
-            getTemplateAndPlace: function(entity, target) {
-                /* assume if we have contents already then we're actually editing, not adding and do nothing */
-                if ($('#'+target).val().length != 0)
+
+            'afterRender:last + $editor.ready': function(e, renderData, $editor) {
+                var value = renderData.data.rawDescription;
+                if (value) {
                     return;
-                $.ajax({
-                    url: appHostAndPath+'/JavaScript/Mashups/Entity%20Templates/'+entity+'.html',
-                    success: function(data,req) {
-                        $('#'+target).val(data);
-                        /* just in case we missed the WYSIWYG editor */
-                        try {
-                            eval('CKEDITOR.instances.'+target+'.setData(data);');
-                        } catch (e) {} /* silance errors if we beat the editor to the punch */
-                    }
-                });
+                }
+
+                var template = getTemplate(renderData.view.config.context);
+                if (!template) {
+                    return;
+                }
+
+                if ($editor.richeditorMarkdown('instance')) {
+                    $editor.richeditorMarkdown('setText', template);
+                }
             }
-        }
-        /* make it happen! */
-        $(document).ready(function() {
-            new entityTemplates().render();
         });
-    }
-)
+    });
